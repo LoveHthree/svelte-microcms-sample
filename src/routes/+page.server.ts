0b6c0dbfbe27046a0a2ae40d950da3898e8ce2a3
LoveHthree <21im0183@i-u.ac.jp>
2022-12-25 15:08:46 +0900

    add blog

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,13 @@
+
+import { getList } from "../lib/microcms";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async () => {
+    return await getList();
+};
+
+export const prerender = true;
+
+
+
+
